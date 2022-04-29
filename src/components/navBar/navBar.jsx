c2ad3ac5c21033ccfd33d645d46db606d6978558
lucaslera91 <lucaslera5@gmail.com>
2022-04-29 14:52:10 -0300

    navBar finalizado

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -16,17 +16,17 @@ function NavBar() {
         toggle ? setBurger('fa-solid fa-xmark') : setBurger('fa-solid fa-bars')
     }
     const reset = (e) => {
-        if (window.innerWidth < 760) {
+        if (window.innerWidth <= 988) {
             click.current.click()
         }
     }
 
     return (
         <div className='navBar'>
-            <Link className='navbar-brand' exact to='/'>
-                <d1 >Los 3 Hermanos</d1>
+            <Link className='navbar-brand' to='/'>
+                <h1 className='d1'>Los 3 Hermanos</h1>
             </Link>
-            <nav className="navbar navbar-expand-md navbar-light">
+            <nav className="navbar navbar-expand-lg navbar-light">
                 {/* <a className="navbar-brand" href="#">Navbar</a> */}
                 <div className='logoContainer'>
                     <button ref={click} onClick={handleBurger} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,20 +37,21 @@ function NavBar() {
 
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <NavLink onClick={reset} className='link' exact to='/'>
-                            <d5>HOME</d5>
+                        <NavLink onClick={reset} className='link' to='/'>
+                            <p className='d5'>HOME</p>
+
                         </NavLink>
                         <NavLink onClick={reset} className='link' to='/conocenos'>
-                            <d5>CONOCENOS</d5>
+                            <p className='d5' className='d5' >CONOCENOS</p>
                         </NavLink>
                         <NavLink onClick={reset} className='link' to='/productos'>
-                            <d5>PRODUCTOS</d5>
+                            <p className='d5' >PRODUCTOS</p>
                         </NavLink>
                         <NavLink onClick={reset} className='link' to='/como_comprar'>
-                            <d5>COMO COMPRAR</d5>
+                            <p className='d5' >COMO COMPRAR</p>
                         </NavLink>
                         <NavLink onClick={reset} className='link' to='/contacto'>
-                            <d5>CONTACTO</d5>
+                            <p className='d5' >CONTACTO</p>
                         </NavLink>
                     </div>
                 </div>
