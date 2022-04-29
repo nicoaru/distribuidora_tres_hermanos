eec3927fc67fdf7041f953566b7f2badeaecba50
lucaslera91 <lucaslera5@gmail.com>
2022-04-29 00:11:35 -0300

    burger button fix

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 // import { CartWidget } from '../../cartWidget/cartWidget';
 import './navBar.css';
 
 
 function NavBar() {
-    //<i class="fa-solid fa-bars"></i>
-    //<i class="fa-solid fa-xmark"></i>
-    //({ isActive }) => isActive ? hamburgerIconClose : hamburgerIcon
+
     const [burger, setBurger] = useState('fa-solid fa-bars');
     const [toggle, setToggle] = useState(true)
+
+    const click = useRef()
 
     const handleBurger = () => {
         setToggle(!toggle)
         toggle ? setBurger('fa-solid fa-xmark') : setBurger('fa-solid fa-bars')
-
+    }
+    const reset = (e) => {
+        click.current.click()
     }
 
     return (
@@ -23,31 +25,29 @@ function NavBar() {
                 {/* <a className="navbar-brand" href="#">Navbar</a> */}
                 <div className='logoContainer'>
                     <Link className='navbar-brand' exact to='/'>
-                        <d1>Los 3 Hermanos</d1>
+                        <d1 onClick={reset}>Los 3 Hermanos</d1>
                     </Link>
 
-                    <button onClick={handleBurger} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <button ref={click} onClick={handleBurger} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <i className={burger}></i>
                     </button>
                 </div>
 
-
-
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <NavLink className='link' exact to='/'>
+                        <NavLink onClick={reset} className='link' exact to='/'>
                             <d5>HOME</d5>
                         </NavLink>
-                        <NavLink className='link' to='/conocenos'>
+                        <NavLink onClick={reset} className='link' to='/conocenos'>
                             <d5>CONOCENOS</d5>
                         </NavLink>
-                        <NavLink className='link' to='/productos'>
+                        <NavLink onClick={reset} className='link' to='/productos'>
                             <d5>PRODUCTOS</d5>
                         </NavLink>
-                        <NavLink className='link' to='/como_comprar'>
+                        <NavLink onClick={reset} className='link' to='/como_comprar'>
                             <d5>COMO COMPRAR</d5>
                         </NavLink>
-                        <NavLink className='link' to='/contacto'>
+                        <NavLink onClick={reset} className='link' to='/contacto'>
                             <d5>CONTACTO</d5>
                         </NavLink>
                     </div>
