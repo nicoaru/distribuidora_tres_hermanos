f03f2398007937960e58cff14e67c4e216452fee
lucaslera91 <lucaslera5@gmail.com>
2022-04-30 14:04:32 -0300

    fix-testing download

---
+++ src/components/homeMainContainer/HomeMainContainer.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import CarrouselMain from '../homeFirstCarousel/CarrouselMain';
+import pdf from '../../img/test.pdf'
+
+
+
+const HomeMainContainer = () => {
+    const [pdfShow, setPdfShow] = useState(false)
+    const open = () => {
+        window.open('http://localhost:3000/precios/30-4')
+    }
+    return (
+        <div>
+            <CarrouselMain />
+            <button onClick={() => { setPdfShow(true) }} className='btn btn-success m-5'>View PDF</button>
+            {pdfShow && <embed src={pdf}
+                type="application/pdf"
+                height="1000"
+                width="800" />}
+            <button onClick={open} className='btn btn-success m-5'>Test</button>
+
+        </div>
+    )
+}
+
+export default HomeMainContainer
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CarrouselMain from '../../components/homeFirstCarousel/CarrouselMain'
+import HomeMainContainer from '../../components/homeMainContainer/HomeMainContainer'
 import './home.css'
 
 function Home() {
     return (
         <div className='home'>
-            <CarrouselMain />
+            <HomeMainContainer />
         </div>
     )
 }
