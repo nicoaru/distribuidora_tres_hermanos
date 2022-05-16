6834f4401388bccd6fb4799a2e4728d03c03a70b
lucaslera91 <lucaslera5@gmail.com>
2022-05-15 23:46:40 -0300

    lista de precios

--- src/components/listadoPrecios/ListaPrecios.jsx
+++ src/components/listadoPrecios/ListaPrecios.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createPDF } from './helperPrecios'
 import './listaPreciosPDF.css'
 import { ProductConsumer } from '../../context/ProductProvider'
+import imgLoader from '../../img/iconos/Ilustraciones_iconos-05.png'
 
 
 
 const ListaPrecios = () => {
 
-    const { listaProductos, imagenes } = ProductConsumer();
+    const { productosQueryDB, getProductsText, getImagenes, imagenes, imagenes2 } = ProductConsumer();
+
+
+    useEffect(() => {
+        productosQueryDB.length < 1 && getProductsText();
+        imagenes.length < 1 && getImagenes();
+    }, [])
 
     const handlePDF = () => {
-
-        createPDF(listaProductos, imagenes)
+        createPDF(productosQueryDB[productosQueryDB.length - 1]?.data, imagenes2)
     }
 
     return (
-        <>
+        <div className='tabla-precios-full'>
+            {!productosQueryDB[productosQueryDB.length - 1]?.data &&
+                <div className="spinner-grow" role="status">
+                    <img src={imgLoader} alt="" />
+                </div>}
             <button onClick={handlePDF} className='btn btn-success m-5'>Descargar PDF</button>
             <table>
                 <thead>
@@ -24,26 +34,27 @@ const ListaPrecios = () => {
                         <th>Imagen</th>
                         <th>Nombre</th>
                         <th>Precio</th>
-                        <th>Descripcion</th>
+                        <th className='tabla-descripcion'>Descripcion</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    {listaProductos.map((element, idx) => {
-                        return (
-                            <tr key={element.id}>
-                                <td>{element.id}</td>
-                                <td><img className="img-thumbnail" src={imagenes[idx]} alt="" /></td>
-                                <td>{element.nombre}</td>
-                                <td>$ {element.precio}</td>
-                                <td>{element.descripcion}</td>
-                            </tr>
-                        )
+                    {productosQueryDB[productosQueryDB.length - 1]?.data.map((element, idx) => {
+                        if (idx !== 0)
+                            return (
+                                <tr key={element.__EMPTY_3}>
+                                    <td>{element.__EMPTY_3}</td>
+                                    <td><img className="img-thumbnail" src={imagenes2[element.__EMPTY_3]} alt="imagen Producto" /></td>
+                                    <td>{element.__EMPTY_4}</td>
+                                    <td>$ {element.__EMPTY_7} / {element.__EMPTY_6}</td>
+                                    <td className='tabla-descripcion'>{element.descripcion}</td>
+                                </tr>
+                            )
                     })}
                 </tbody>
             </table>
 
-        </>
+        </div>
     )
 }
 
